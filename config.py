2d7a2f58ca227697b217154603fd1947b23fa800
levelingdragon <77844089+levelingdragon@users.noreply.github.com>
2021-12-16 19:38:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 bot_token = "1298391278:aksjasjkakj-lmaosdjsahjhshjas"
-sudoers = ["@Tamilvip007", 1234567890, @TheSungJinWoo]
+sudoers = ["@Tamilvip007", 1234567890, "@TheSungJinWoo"]
 root = 2079472115
 spammers = [1234567890, 123456789]
 WELCOME_DELAY_KICK_SEC = 300
