fd75ff387c3fa0492c014455df69d40c83ca3198
Sung Jin-Woo <77414846+Solo-Dragon@users.noreply.github.com>
2021-12-16 20:24:36 +0530

    ok

--- main.py
+++ main.py
@@ -204,6 +204,74 @@ async def welcome(_, message: Message):
     await message.reply_animation("CgACAgIAAx0CVsThkQABAsD8Ybs-GL3regFniHZpNzwQGe9aU3MAApgRAAJdrMFIb82stu3-1C8jBA")
     await kick_restricted_after_delay(WELCOME_DELAY_KICK_SEC, button_message)  
   
+        
+        
+@app.on_callback_query(filters.regex("pressed_button"))
+async def callback_query_welcome_button(_, callback_query):
+    """After the new member presses the button, set his permissions to
+    chat permissions, delete button message and join message
+    """
+    button_message = callback_query.message
+    join_message = button_message.reply_to_message
+    pending_user = join_message.from_user
+    pending_user_id = pending_user.id
+    pressed_user_id = callback_query.from_user.id
+    if pending_user_id == pressed_user_id:
+        await callback_query.answer("Congrats, captcha passed!")
+        await button_message.chat.unban_member(pending_user_id)
+        await button_message.delete()
+    else:
+        await callback_query.answer("This is not for you")
+        
+sync def kick_restricted_after_delay(delay, button_message: Message):
+    """If the new member is still restricted after the delay, delete
+    button message and join message and then kick him
+    """
+    await asyncio.sleep(delay)
+    join_message = button_message.reply_to_message
+    group_chat = button_message.chat
+    user_id = join_message.from_user.id
+    await join_message.delete()
+    await button_message.delete()
+    await _ban_restricted_user_until_date(group_chat, user_id, duration=delay)
+
+
+@app.on_message(filters.left_chat_member)
+async def left_chat_member(_, message: Message):
+    """When a restricted member left the chat, ban him for a while"""
+    group_chat = message.chat
+    user_id = message.left_chat_member.id
+    await _ban_restricted_user_until_date(group_chat, user_id,
+                                          duration=WELCOME_DELAY_KICK_SEC)
+
+
+async def _ban_restricted_user_until_date(group_chat,
+                                          user_id: int,
+                                          duration: int):
+    try:
+        member = await group_chat.get_member(user_id)
+        if member.status == "restricted":
+            until_date = int(datetime.utcnow().timestamp() + duration)
+            await group_chat.kick_member(user_id, until_date=until_date)
+    except UserNotParticipant:
+        pass
+
+# eval
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
+
+
+async def edit_or_reply(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
+                
 # --- BASH EVENT HERE --- #
 @client.on(events.NewMessage(chats=auth_chts, pattern="/bash ?(.*)"))
 async def bashE(event):
